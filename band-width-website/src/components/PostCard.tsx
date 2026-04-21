0c7c8e0fedcbd644d52771a7cc7e9a2e8db9d5f6
Pipervetowich <111729427+Pipervetowich@users.noreply.github.com>
2026-04-21 01:13:59 -0600

    Update PostCard.tsx

--- band-width-website/src/components/PostCard.tsx
+++ band-width-website/src/components/PostCard.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import type { Post, CategoryColor } from "../types";
-import styles from "./PostCard.module.css";
+import styles from "./Postcard.module.css";
 
 interface Props {
   post: Post;
